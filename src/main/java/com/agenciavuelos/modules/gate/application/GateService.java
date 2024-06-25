5e2427218395346c71542d2811e5343278df6259
lipaocaspi <66758443+lipaocaspi@users.noreply.github.com>
2024-06-24 21:07:56 -0500

    feat: :sparkles: create CRUD

--- src/main/java/com/agenciavuelos/modules/gate/application/GateService.java
+++ src/main/java/com/agenciavuelos/modules/gate/application/GateService.java
@@ -1,5 +1,51 @@
 package com.agenciavuelos.modules.gate.application;
 
-public class GateService {
+import java.util.List;
+import java.util.Optional;
 
+import com.agenciavuelos.modules.airport.domain.Airport;
+import com.agenciavuelos.modules.airport.infrastructure.AirportRepository;
+import com.agenciavuelos.modules.gate.domain.Gate;
+import com.agenciavuelos.modules.gate.infrastructure.GateRepository;
+
+public class GateService {
+    private final GateRepository gateRepository;
+    private final AirportRepository airportRepository;
+
+    public GateService(GateRepository gateRepository, AirportRepository airportRepository) {
+        this.gateRepository = gateRepository;
+        this.airportRepository = airportRepository;
+    }
+
+    public List<Gate> findAllGates(){
+        return gateRepository.findAll();
+    }
+
+    public Optional<Gate>  findGateById(int id) {
+        Optional<Gate> optionalGate = this.gateRepository.findById(id);
+        return optionalGate;
+
+    }
+
+    public void deleteGate(int id){
+        this.gateRepository.delete(id);
+    }
+
+    public void updateGate(Gate gate){
+        this.gateRepository.update(gate);
+    }
+
+    public void createGate(Gate gate){
+        this.gateRepository.save(gate);
+    }
+
+    public String getAirportId(String id) {
+        String idF = "";
+        Optional<Airport> foundAirport = airportRepository.findById(id);
+        if (foundAirport.isPresent()) {
+            Airport airport = foundAirport.get();
+            idF = airport.getId();
+        }
+        return idF;
+    }
 }
--- src/main/java/com/agenciavuelos/modules/gate/infrastructure/GateRepository.java
+++ src/main/java/com/agenciavuelos/modules/gate/infrastructure/GateRepository.java
@@ -1,5 +1,18 @@
 package com.agenciavuelos.modules.gate.infrastructure;
 
-public interface GateRepository {
+import java.util.List;
+import java.util.Optional;
 
+import com.agenciavuelos.modules.gate.domain.Gate;
+
+public interface GateRepository {
+    public Optional<Gate> findById(int id);
+
+    public List<Gate> findAll();
+
+    public void save(Gate gate);
+
+    public void update(Gate gate);
+
+    public void delete(int id);
 }
