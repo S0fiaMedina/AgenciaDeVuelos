c1e464f080a0de21d6a60ab6ec051f9280b2e180
lipaocaspi <66758443+lipaocaspi@users.noreply.github.com>
2024-06-24 21:09:06 -0500

    feat: :sparkles: create CRUD

---
+++ src/main/java/com/agenciavuelos/modules/tripulationRole/TestMain.java
@@ -0,0 +1,29 @@
+package com.agenciavuelos.modules.tripulationRole;
+
+import com.agenciavuelos.Console.Initializer;
+
+public class TestMain {
+
+    public static void main(String[] args) {
+        // No se si sea necesario realizar una clase de menus
+
+    String url = "jdbc:mysql://localhost:3306/AGENCIA_VUELOS";
+    String user = "";
+    String password = "";
+    Initializer initializer = new Initializer(url, user, password);
+
+    // inicializados
+    
+    initializer.startTripulationRoleConsoleAdapter().run();
+
+    /**
+     * INSERTS DE PRUEBA
+    INSERT INTO manufacturer (name) VALUES ('Boeing');
+    INSERT INTO manufacturer (name) VALUES ('Airbus');
+    INSERT INTO manufacturer (name) VALUES ('Lockheed Martin');
+    INSERT INTO manufacturer (name) VALUES ('Embraer');
+    INSERT INTO manufacturer (name) VALUES ('Bombardier');
+    */
+    }
+    
+}
--- src/main/java/com/agenciavuelos/modules/tripulationRole/domain/TripulationRole.java
+++ src/main/java/com/agenciavuelos/modules/tripulationRole/domain/TripulationRole.java
@@ -1,5 +1,31 @@
 package com.agenciavuelos.modules.tripulationRole.domain;
 
 public class TripulationRole {
+    private int id; 
+    private String name;
 
+    public TripulationRole(String name) {
+        this.name = name;
+    }
+
+    public TripulationRole(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
