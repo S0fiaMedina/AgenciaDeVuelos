5e28c3caa2bcef7261e68c91572bacda9dafe782
S0fiaMedina <151967289+S0fiaMedina@users.noreply.github.com>
2024-06-26 09:29:03 -0500

    chore: add new branch for connextions

--- src/main/java/com/agenciavuelos/modules/flightConnection/domain/FlightConnection.java
+++ src/main/java/com/agenciavuelos/modules/flightConnection/domain/FlightConnection.java
@@ -1,5 +1,11 @@
 package com.agenciavuelos.modules.flightConnection.domain;
 
 public class FlightConnection {
-
+    private int id;
+    private int connectionNumber;
+    private int idTrip;
+    private int idAirport;
+    // se pregunta al usuario si desea un vuelo directo o de escalas
+    // si es directo... SE HACE SOLO UNA CONEXION 
+    // 
 }
